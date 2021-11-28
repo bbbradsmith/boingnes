c601c7ded6127a5dd89888de1fc1f40f1697691c
bbbradsmith <bbbradsmith@users.noreply.github.com>
2021-11-28 07:36:53 -0500

    document nes library stuff atari st background grid tweak

--- boing.c
+++ boing.c
@@ -4,6 +4,12 @@
 // http://rainwarrior.ca
 //
 
+#include "temp/sprite.h" // sprite enums
+
+//
+// NES library
+//
+
 typedef unsigned char      uint8;
 typedef   signed char      sint8;
 typedef unsigned short int uint16;
@@ -11,21 +17,13 @@ typedef   signed short int sint16;
 typedef unsigned long  int uint32;
 typedef   signed long  int sint32;
 
-// Boing Ball data
-
-#include "temp/sprite.h"
-extern uint8 amiga_nmt[1024];
-extern uint8 atari_nmt[1024];
-
-// NES library
-
-extern uint8 nmi_count;
-extern uint8 input[2];
-extern uint8 input_new[2];
-extern uint8 ascii_offset;
-extern uint8 cnrom_bank;
-extern uint8 i,j,k,l;
-extern uint16 mx,nx,ox,px;
+extern uint8 nmi_count;    // every NMI increments this value
+extern uint8 input[2];     // gamepad inputs
+extern uint8 input_new[2]; // new buttons pressed at last poll
+extern uint8 ascii_offset; // offset from CHR index to ASCII in ppu_string
+extern uint8 cnrom_bank;   // applies CNROM CHR bank at next o
+extern uint8 i,j,k,l;      // convenient zeropage temporaries
+extern uint16 mx,nx,ox,px; // 16-bit ZP temporaries
 #pragma zpsym("nmi_count")
 #pragma zpsym("input")
 #pragma zpsym("input_new")
@@ -40,39 +38,49 @@ extern uint16 mx,nx,ox,px;
 #pragma zpsym("ox")
 #pragma zpsym("px")
 
-extern uint8 palette[32];
-extern uint8 ppu_send[72];
-extern uint8 oam[256];
+extern uint8 palette[32];  // NES render palette
+extern uint8 ppu_send[72]; // data packets uploaded to PPU (see ppu_packet)
+extern uint8 oam[256];     // sprite OAM data
 
-extern void ppu_latch(uint16 addr);
-extern void ppu_latch_at(uint8 x, uint8 y);
-extern void ppu_write(uint8 value);
-extern void ppu_load(const uint8* data, uint16 count);
-extern void ppu_fill(uint8 value, uint16 count);
-extern void ppu_string(const char* s);
+// PPU updates while rendering is off
+extern void ppu_latch(uint16 addr); // sets PPU write address
+extern void ppu_latch_at(uint8 x, uint8 y); // latches a nametable tile (x>32=$2400, y>32=$2800, etc.)
+extern void ppu_write(uint8 value); // writes one byte at latched address
+extern void ppu_load(const uint8* data, uint16 count); // writes block of data
+extern void ppu_fill(uint8 value, uint16 count); // writes repeating byte
+extern void ppu_string(const char* s); // writes a string (set ascii_offset before using)
+
+// PPU updates while rendering is on, during vblank
 extern void ppu_packet(uint16 addr, const uint8* data, uint8 count);
-extern void ppu_packet_apply();
-extern void ppu_ctrl(uint8 v);
-extern void ppu_control_apply(uint8 v);
-extern void ppu_mask(uint8 v);
+extern void ppu_packet_apply(); // immediately apply packets while rendering is off
+// Internal packet format, stored in ppu_send:
+//   uint8: count (number of bytes to update), 0 = end of packets
+//   uint16: address (PPU address for data)
+//   uint8 * count: bytes of data
+
+// PPU controls
+extern void ppu_ctrl(uint8 v); // new value for $2000 at next render
+extern void ppu_control_apply(uint8 v); // set immediately
+extern void ppu_mask(uint8 v); // $2001 at next render
 extern void ppu_mask_apply(uint8 v);
-extern void ppu_scroll(uint16 x, uint16 y);
-extern void ppu_render_frame();
-extern void ppu_wait_frame();
-extern void ppu_render_off();
-extern void ppu_nmi_off();
+extern void ppu_scroll(uint16 x, uint16 y); // scroll at next render (note 240<=y<256 is "between" nametables)
+extern void ppu_render_frame(); // send updates to renderer and wait for next frame
+extern void ppu_wait_frame(); // wait 1 frame without applying updates
+extern void ppu_render_off(); // turn renderer off
+extern void ppu_nmi_off(); // disable NMI
 extern void ppu_nmi_on();
 
-extern void sprite_begin();
-extern void sprite_end();
-extern void sprite_tile(uint8 t, uint8 a, uint8 x, uint8 y);
-extern void sprite(uint8 e, uint8 x, uint8 y);
-extern void sprite_flip(uint8 e, uint8 x, uint8 y);
+// Sprites
+extern void sprite_begin(); // begin a new sprite list
+extern void sprite_end(); // conclude the sprite list
+extern void sprite_tile(uint8 t, uint8 a, uint8 x, uint8 y); // add a single tile
+extern void sprite(uint8 e, uint8 x, uint8 y); // add sprite "e" at x/y
+extern void sprite_flip(uint8 e, uint8 x, uint8 y); // sprite with horizontal flip
 
-extern void sound_play(const uint8* sound);
-extern uint8 input_poll();
-extern uint8 input_poll2();
-void nescopy(void* dst, const void* src, uint8 count);
+extern void sound_play(const uint8* sound); // play the sound data
+extern uint8 input_poll(); // poll the first player gamepad (result also in input/input_new)
+extern uint8 input_poll2(); // poll both gamepads
+void nescopy(void* dst, const void* src, uint8 count); // copies bytes (memcpy replacement), but count is <256
 
 #define PAD_A       0x80
 #define PAD_B       0x40
@@ -83,22 +91,11 @@ void nescopy(void* dst, const void* src, uint8 count);
 #define PAD_L       0x02
 #define PAD_R       0x01
 
-// palettes
-
-const uint8 pal[32] = {
-	0x0F, 0x00, 0x04, 0x24, // Amiga BG
-	0x0F, 0x0B, 0x2B, 0x3B, // Atari ST BG
-	0x0F, 0x01, 0x11, 0x22, // (filler blue)
-	0x0F, 0x03, 0x13, 0x23, // (filler purple)
-	0x0F, 0x0F, 0x16, 0x30, // ball red-white
-	0x0F, 0x0F, 0x30, 0x16, // ball white-red
-	0x0F, 0x07, 0x17, 0x27, // (filler yellow)
-	0x0F, 0x09, 0x19, 0x29, // (filler green)
-};
-
+//
 // NES library test (press SELECT+START to activate)
+//
 
-const uint8 hextest[] = {
+const uint8 hextest[] = { // 0123456789ABCDEF
 	0x10,0x11,0x12,0x13,0x14,0x15,0x16,0x17,0x18,0x19,
 	0x21,0x22,0x23,0x24,0x25,0x26 };
 
@@ -111,21 +108,22 @@ void libtest()
 	static uint8 hexpackets[6];
 
 	cnrom_bank = 0;
-	ascii_offset = 0x20;
-	ppu_latch(0x2000);
+	ascii_offset = 0x20; // $20 subtracted from ASCII to get CHR tile ($20 ' ' is located at 0)
+	ppu_latch(0x2000); // empty first screen
 	ppu_fill(0,1024);
-	ppu_latch(0x2400);
-	for (mx = 0; mx<1024; ++mx) ppu_write(mx & 0xFF);
+	ppu_latch(0x2C00); // second screen displays all tiles / all palettes
+	for (mx = 0; mx<(1024-64); ++mx) ppu_write(mx & 0xFF);
+	for (i=0; i<4; ++i) ppu_fill(i*0x55,16);
 	ppu_latch_at(4,4);
 	ppu_string("NES Library Test");
 	ppu_latch_at(32+8,32+22); // goes to "bottom right" screen
 	ppu_string("OFFSCREEN");
-	ppu_packet(0x2000+(6*32)+4,hextest,sizeof(hextest));
+	ppu_packet(0x2000+(6*32)+4,hextest,sizeof(hextest)); // test packet_apply with the hexadecimal string
 	ppu_packet_apply();
 	while(1)
 	{
-		input_poll2();
-		for (i=0;i<2;++i)
+		input_poll2(); // poll both gamepads
+		for (i=0;i<2;++i) // d-pad moves 2 sprites around the screen
 		{
 			if (input[i] & PAD_L) --bx[i];
 			if (input[i] & PAD_R) ++bx[i];
@@ -134,7 +132,7 @@ void libtest()
 		}
 		sprite_begin();
 		sprite(     SPRITE_boing,bx[0],by[0]);
-		sprite_flip(SPRITE_boing,bx[1],by[1]);
+		sprite_flip(SPRITE_boing,bx[1],by[1]); // 2P sprite is flipped
 		sprite_end();
 		hexpackets[0] = hextest[bx[0]     >> 4];
 		hexpackets[2] = hextest[by[0]     >> 4];
@@ -142,34 +140,53 @@ void libtest()
 		hexpackets[1] = hextest[bx[0]     & 0xF];
 		hexpackets[3] = hextest[by[0]     & 0xF];
 		hexpackets[5] = hextest[nmi_count & 0xF];
-		ppu_packet(0x2000+( 8*32)+4,hexpackets+0,2);
-		ppu_packet(0x2000+( 9*32)+4,hexpackets+2,2);
-		ppu_packet(0x2000+(10*32)+4,hexpackets+4,2);
-		if (input[0] & PAD_B) ++tsx;
+		ppu_packet(0x2000+( 8*32)+4,hexpackets+0,2); // P1 X
+		ppu_packet(0x2000+( 9*32)+4,hexpackets+2,2); // P1 Y
+		ppu_packet(0x2000+(10*32)+4,hexpackets+4,2); // nmi_count (observes NMI activity)
+		if (input[0] & PAD_B) ++tsx; // B/A scrolls screen
 		if (input[0] & PAD_A) ++tsy;
-		if (input[0] & PAD_SELECT)
+		if (input[0] & PAD_SELECT) // SELECT shows immediate greyscale at scanline end of frame activity
 		{
 			ppu_mask_apply(0x1F); // greyscale immediately
 			ppu_mask(0x1E); // normal render at next NMI
 		}
-		if (input[1] & PAD_B) ppu_wait_frame(); // slow to half
-		if (input_new[1] & PAD_A) // suspend NMI (can watch counter stop/resume)
+		if (input[1] & PAD_B) ppu_wait_frame(); // P2 B = slow to half speed (note nmi_count maintains pace)
+		if (input_new[1] & PAD_A) // P2 A = suspend NMI (note nmi_count does stop/resume)
 		{
 			ppu_nmi_off();
 			input_poll2();
-			while (!((input_new[0] | input_new[1]) & PAD_A))
+			while (!((input_new[0] | input_new[1]) & PAD_A)) // resume when A pressed again
 				input_poll2();
 			ppu_nmi_on();
 		}
-		ppu_scroll(tsx,tsy);
+		ppu_scroll(tsx,tsy); // test scroll
 		ppu_render_frame();
-		if ((input_new[0] | input_new[1]) & PAD_START) break;
+		if ((input_new[0] | input_new[1]) & PAD_START) break; // START exits test
 	}
 	ppu_render_off();
 }
 
+//
 // Boing Ball
+//
 
+// data
+extern uint8 amiga_nmt[1024];
+extern uint8 atari_nmt[1024];
+
+// palettes
+const uint8 pal[32] = {
+	0x0F, 0x00, 0x04, 0x24, // Amiga BG
+	0x0F, 0x0B, 0x2B, 0x3B, // Atari ST BG
+	0x0F, 0x01, 0x11, 0x22, // (filler blue)
+	0x0F, 0x03, 0x13, 0x23, // (filler purple)
+	0x0F, 0x0F, 0x16, 0x30, // ball red-white
+	0x0F, 0x0F, 0x30, 0x16, // ball white-red
+	0x0F, 0x07, 0x17, 0x27, // (filler yellow)
+	0x0F, 0x09, 0x19, 0x29, // (filler green)
+};
+
+// variables
 uint8 boing_bg = 0;
 uint8 boing_par = 0;
 
@@ -183,6 +200,7 @@ uint16 boing_vr = 256/2; // spin rate
 
 uint8 boing_sprite = SPRITE_squ0000;
 
+// redraw backgrounds while rendering is off
 void boing_redraw()
 {
 	ppu_latch(0x2000);
@@ -190,6 +208,7 @@ void boing_redraw()
 	ppu_scroll(0,0);
 }
 
+// animated ball
 void boing_animate()
 {
 	boing_sprite = boing_par ? SPRITE_pau0000 : SPRITE_squ0000;
@@ -200,7 +219,7 @@ void boing_animate()
 	while (boing_r >= (128<<8)) boing_r += (12<<8); // wrap negative 0-11
 	while (boing_r >= ( 12<<8)) boing_r -= (12<<8); // wrap positive 0-11
 	
-	// TODO vx/vy and bounce sound
+	// TODO vx/vy and bounce sounds
 
 	// TODO controls
 	
@@ -211,7 +230,6 @@ void boing_animate()
 }
 
 // main loop
-
 void main()
 {
 	nescopy(palette,pal,32);
@@ -238,6 +256,7 @@ void main()
 		// B/A to select pixel-aspect-ratio
 		if (input_new[0] & PAD_B) boing_par = 1;
 		if (input_new[0] & PAD_A) boing_par = 0;
+		// TODO left/right up/down
 		boing_animate();
 		ppu_render_frame();
 	}
